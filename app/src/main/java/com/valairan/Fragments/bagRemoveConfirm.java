5fc84be273e215e8a0781572c785bb16192eee46
valairan <darylvalairan@outlook.com>
2021-08-15 00:23:50 +0530

    Almost Done

--- app/src/main/java/com/valairan/Fragments/bagRemoveConfirm.java
+++ app/src/main/java/com/valairan/Fragments/bagRemoveConfirm.java
@@ -17,12 +17,10 @@ import com.valairan.inventory.R;
  */
 public class bagRemoveConfirm extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
 
